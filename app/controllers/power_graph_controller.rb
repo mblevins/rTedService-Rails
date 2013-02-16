8cdb6b3abc0e07de55da8c6db1676f0262edcdd3
Mike Blevins <mike@wabbitinc.com>
2013-02-16 10:44:35 -0800

    Added graph options

--- app/controllers/power_graph_controller.rb
+++ app/controllers/power_graph_controller.rb
@@ -2,6 +2,14 @@ class PowerGraphController < ApplicationController
   def index
       @dayPowersResult = DayPower.limit(365).order( "day DESC" )
       
+    @graphType = params[:graphType]
+    if (@graphType == nil) then
+      @graphType = "bar"
+    elsif (@graphType != "time" and @graphType != "bar" and @graphType != "table")
+      # sleazy...
+      @graphType = "bar"
+    end
+    
     render :template => 'power_graph/index.html.erb'
   end
 
